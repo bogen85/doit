52f6227b78c4b6680f767a489f3770ffa9c9d507
Bogen85 <bogen85@teegra.net>
2024-01-02 02:21:36 -0600

    alias -> cmd_name

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+// code: language=Rust insertSpaces=true tabSize=2
+
 use std::env;
 use std::fs::File;
 use std::io::Read;
@@ -6,9 +8,11 @@ use std::process::Command;
 use toml_edit::Document;
 use toml_edit::Table;
 
+static SUB_COMMAND: &str = ".sub_command";
+
 fn print_help() {
-  println!("Usage: doit <alias> [args...]");
-  println!("       doit --help <alias>");
+  println!("Usage: doit <command> [args...]");
+  println!("       doit --help <command>");
 }
 
 fn read_doit_file() -> String {
@@ -20,12 +24,12 @@ fn read_doit_file() -> String {
   contents
 }
 
-fn print_alias_help(alias: &str) {
+fn print_alias_help(cmd_name: &str) {
   let contents = read_doit_file();
 
   let doc = contents.parse::<Document>().expect("Unable to parse TOML");
 
-  if let Some(table) = doc[alias].as_table() {
+  if let Some(table) = doc[cmd_name].as_table() {
     let command = table["command"].as_str().expect("Missing command");
     let description = if table.contains_key("description") {
       table["description"]
@@ -45,7 +49,7 @@ fn print_alias_help(alias: &str) {
       String::new()
     };
 
-    println!("Alias: {}", alias);
+    println!("Alias: {}", cmd_name);
     println!("Command: {}", command);
     println!("Arguments: {}", args);
     println!("Description: {}", description);
@@ -54,12 +58,16 @@ fn print_alias_help(alias: &str) {
   }
 }
 
-fn process_cmd(alias: &str, table: &Table, additional_args: &[String]) {
+fn process_cmd(cmd_name: &str, table: &Table, additional_args: &[String]) {
   if table.contains_key("pre") {
-    println!("Runing {}.pre", alias);
+    println!("Runing {}.pre", cmd_name);
     if let Some(sub_table) = table["pre"].as_table() {
-      process_cmd(alias, &sub_table, &[]);
+      process_cmd(cmd_name, &sub_table, &[]);
     }
+  }
+
+  if cmd_name != SUB_COMMAND {
+    println!("Runing command {}", cmd_name);
   }
 
   let command = table["command"].as_str().expect("Missing command");
@@ -86,9 +94,9 @@ fn process_cmd(alias: &str, table: &Table, additional_args: &[String]) {
 
   if rc == 0 {
     if table.contains_key("post") {
-      println!("Runing {}.post", alias);
+      println!("Runing {}.post", cmd_name);
       if let Some(sub_table) = table["post"].as_table() {
-        process_cmd(alias, &sub_table, &[]);
+        process_cmd(SUB_COMMAND, &sub_table, &[]);
       }
     }
   } else {
@@ -108,16 +116,15 @@ fn main() {
     return;
   }
 
-  let alias = &args[1];
+  let cmd_name = &args[1];
   let additional_args = if args.len() > 2 { &args[2..] } else { &[] };
 
   let contents = read_doit_file();
 
   let doc = contents.parse::<Document>().expect("Unable to parse TOML");
-  if doc.contains_key(alias) {
-    if let Some(table) = doc[alias].as_table() {
-      println!("Runing command {}", alias);
-      process_cmd(alias, &table, additional_args);
+  if doc.contains_key(cmd_name) {
+    if let Some(table) = doc[cmd_name].as_table() {
+      process_cmd(cmd_name, &table, additional_args);
     }
   } else {
     println!("Alias not found");
