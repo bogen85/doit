1bac73b21abbc7c3e821d1bb56c3c527fa40d4b3
Bogen85 <bogen85@teegra.net>
2024-01-02 13:00:54 -0600

    added --cmds to list commands

--- src/main.rs
+++ src/main.rs
@@ -10,21 +10,20 @@ use toml_edit::{Array, Document, Table};
 fn print_help() {
   println!("Usage: doit <command> [args...]");
   println!("       doit --help <command>");
+  println!("       doit --cmds");
 }
 
-fn read_doit_file() -> String {
+fn read_doit_file() -> Document {
   let mut contents = String::new();
   File::open("doit.toml")
     .expect("Unable to open file")
     .read_to_string(&mut contents)
     .expect("Unable to read file");
-  contents
+  contents.parse::<Document>().expect("Unable to parse TOML")
 }
 
 fn print_alias_help(cmd_name: &str) {
-  let contents = read_doit_file();
-
-  let doc = contents.parse::<Document>().expect("Unable to parse TOML");
+  let doc = read_doit_file();
 
   if let Some(table) = doc[cmd_name].as_table() {
     let command = table["command"].as_str().expect("Missing command");
@@ -35,7 +34,7 @@ fn print_alias_help(cmd_name: &str) {
     } else {
       "No description provided"
     };
-    let args = if table.contains_key("description") {
+    let args = if table.contains_key("args") {
       let toml_args = table["args"].as_array();
       toml_args
         .iter()
@@ -48,7 +47,7 @@ fn print_alias_help(cmd_name: &str) {
 
     println!("Alias: {}", cmd_name);
     println!("Command: {}", command);
-    println!("Arguments: {}", args);
+    println!("Arguments:{}", args);
     println!("Description: {}", description);
   } else {
     println!("Alias not found");
@@ -140,6 +139,12 @@ fn process_cmd(cmd_name: &str, table: &Table, additional_args: &[String]) {
   }
 }
 
+fn list_cmds() {
+  for (i, (cmd, _)) in read_doit_file().as_table().iter().enumerate() {
+    println!("{}: {}", i + 1, cmd);
+  }
+}
+
 fn main() {
   let args: Vec<String> = env::args().collect();
   if args.len() < 2 || args[1] == "--help" {
@@ -150,13 +155,15 @@ fn main() {
     }
     return;
   }
+  if args.len() < 2 || args[1] == "--cmds" {
+    list_cmds();
+    return;
+  }
 
   let cmd_name = &args[1];
   let additional_args = if args.len() > 2 { &args[2..] } else { &[] };
 
-  let contents = read_doit_file();
-
-  let doc = contents.parse::<Document>().expect("Unable to parse TOML");
+  let doc = read_doit_file();
   if doc.contains_key(cmd_name) {
     if let Some(table) = doc[cmd_name].as_table() {
       process_cmd(cmd_name, &table, additional_args);
