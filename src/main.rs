d4dbd5fb60987a0e361377c1f331d20f7b27379d
Bogen85 <bogen85@teegra.net>
2024-01-02 20:02:26 -0600

    more formatting options set. Added cargo-do as an alias for doit

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,7 @@ use toml_edit::{Array, Document, Table};
 
 fn read_doit_file() -> Document {
   let mut contents = String::new();
-  File::open("doit.toml")
-    .expect("Unable to open file")
-    .read_to_string(&mut contents)
-    .expect("Unable to read file");
+  File::open("doit.toml").expect("Unable to open file").read_to_string(&mut contents).expect("Unable to read file");
   contents.parse::<Document>().expect("Unable to parse TOML")
 }
 
@@ -27,40 +24,31 @@ fn render_template(table: &Table, template: &str) -> String {
 }
 
 fn process_pre_post_cmd(which: &str, cmd_name: &str, table: &Table) {
-  let sub_args = table[which]
-    .as_array()
-    .expect(&format!("{} is not an array", which));
+  let sub_args = table[which].as_array().expect(&format!("{} is not an array", which));
 
   for (index, args_in) in sub_args.iter().enumerate() {
     println!("Running command {}:{}:{}", cmd_name, which, index + 1);
 
     let args = {
-      let vec_in = args_in
-        .as_array()
-        .expect(&format!("{}[{}] is not an array", which, index));
+      let vec_in = args_in.as_array().expect(&format!("{}[{}] is not an array", which, index));
       let mut vec: Vec<String> = Vec::new();
       for arg in vec_in {
         vec.push(render_template(
           table,
-          &arg
-            .as_str()
-            .expect(&format!("Invalid argument for {}:[{}]", which, index))
-            .to_string(),
+          &arg.as_str().expect(&format!("Invalid argument for {}:[{}]", which, index)).to_string(),
         ));
       }
       vec
     };
 
-    let cmd = args
-      .get(0)
-      .expect(&format!("{}[{}] array is empty", which, index));
+    let cmd = args.get(0).expect(&format!("{}[{}] array is empty", which, index));
     let cmd_args = if args.len() > 1 { &args[1..] } else { &[] };
 
     let exit_status = {
-      let mut child = Command::new(&cmd).args(&*cmd_args).spawn().expect(&format!(
-        "Failed to execute sub command: {}:{:?}",
-        cmd, cmd_args
-      ));
+      let mut child = Command::new(&cmd)
+        .args(&*cmd_args)
+        .spawn()
+        .expect(&format!("Failed to execute sub command: {}:{:?}", cmd, cmd_args));
       child.wait()
     };
     let rc = exit_status.expect("RC").code().unwrap_or(1);
@@ -77,35 +65,23 @@ fn process_cmd(cmd_name: &str, table: &Table, additional_args: &[String]) {
 
   println!("Running command {}", cmd_name);
 
-  let command = table["command"]
-    .as_str()
-    .expect(&format!("{}: missing command", cmd_name));
+  let command = table["command"].as_str().expect(&format!("{}: missing command", cmd_name));
 
   let args = {
     let mut args = vec![command.to_string()];
 
-    let toml_args: Array = if table.contains_key("args") {
-      table["args"].as_array().expect("Array!").clone()
-    } else {
-      Array::default()
-    };
+    let toml_args: Array =
+      if table.contains_key("args") { table["args"].as_array().expect("Array!").clone() } else { Array::default() };
     for arg in toml_args {
-      args.push(render_template(
-        table,
-        arg.as_str().expect("Invalid argument"),
-      ));
+      args.push(render_template(table, arg.as_str().expect("Invalid argument")));
     }
     args.extend_from_slice(additional_args);
     args
   };
 
   let exit_status = {
-    let cmd = &args[0];
-    let argv = &args[1..];
-    let mut child = Command::new(cmd)
-      .args(argv)
-      .spawn()
-      .expect(&format!("Failed to execute command: {:?}", args));
+    let mut child =
+      Command::new(&args[0]).args(&args[1..]).spawn().expect(&format!("Failed to execute command: {:?}", args));
     child.wait()
   };
   let rc = exit_status.expect("RC").code().unwrap_or(1);
@@ -159,19 +135,13 @@ fn show_details(cmd_name: &str) -> bool {
   if let Some(table) = doc[cmd_name].as_table() {
     let command = table["command"].as_str().expect("Missing command");
     let description = if table.contains_key("description") {
-      table["description"]
-        .as_str()
-        .unwrap_or("No description provided")
+      table["description"].as_str().unwrap_or("No description provided")
     } else {
       "No description provided"
     };
     let args = if table.contains_key("args") {
       let toml_args = table["args"].as_array();
-      toml_args
-        .iter()
-        .map(|arg| render_template(table, &arg.to_string()))
-        .collect::<Vec<_>>()
-        .join(" ")
+      toml_args.iter().map(|arg| render_template(table, &arg.to_string())).collect::<Vec<_>>().join(" ")
     } else {
       String::new()
     };
@@ -187,7 +157,19 @@ fn show_details(cmd_name: &str) -> bool {
 }
 
 fn main() {
-  let args: Vec<String> = env::args().collect();
+  let args = {
+    let mut args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+      match args[1].as_str() {
+        | "doit" | "do" => {
+          args.remove(1);
+        },
+        | _ => {},
+      }
+    }
+    args
+  };
+
   let program = args[0].clone();
 
   let opts = {
@@ -200,10 +182,10 @@ fn main() {
   };
 
   let matches = match opts.parse(&args[1..]) {
-    Ok(m) => m,
-    Err(f) => {
+    | Ok(m) => m,
+    | Err(f) => {
       panic!("{}", f.to_string())
-    }
+    },
   };
 
   if matches.opt_present("help") {
@@ -234,11 +216,7 @@ fn main() {
     exit(1);
   };
 
-  let additional_args = if matches.free.len() > 1 {
-    matches.free[1..].to_vec()
-  } else {
-    vec![]
-  };
+  let additional_args = if matches.free.len() > 1 { matches.free[1..].to_vec() } else { vec![] };
   if !primary(&cmd_name, &additional_args) {
     println!("{} not found", cmd_name);
     print_usage(&program, opts);
